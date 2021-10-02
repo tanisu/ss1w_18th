8a9603f2860d9e24599353e6bc82576ffbe5e9e6
okitayo <84880971+okitayo@users.noreply.github.com>
2021-10-02 20:18:47 +0900

    何も進んではいない、エラー

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class player : MonoBehaviour
 {
-    //新しいインプットシステム調べる
-    //レイを飛ばして、返ってくるか
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-    public void OnMove(InputAction.CallbackContext context)
-	{
-        Debug.Log(context.ReadValue<Vector2>());
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
